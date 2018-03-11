8f923c56166c65896af47f30e9e223c832cd796e
Gurpreet Juttla <gurpreet.juttla@gmail.com>
2018-03-11 18:04:15 +0100

    Remove unused import

--- src/LinzLinienAlexaSkill.Web/Startup.cs
+++ src/LinzLinienAlexaSkill.Web/Startup.cs
@@ -5,7 +5,6 @@ using LinzLinienAlexaSkill.Web.Utility;
 using LinzLinienEfa.Service.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
