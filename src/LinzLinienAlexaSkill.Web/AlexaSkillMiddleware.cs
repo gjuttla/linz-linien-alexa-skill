fc51015e324c0bf85ebe5eb93a6ad9ecdf5248f4
Gurpreet Juttla <gurpreet.juttla@gmail.com>
2018-06-17 15:52:25 +0200

    Implement Middleware for Alexa Skill request

---
+++ src/LinzLinienAlexaSkill.Web/AlexaSkillMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using LinzLinienAlexaSkill.Web.Alexa;
+using LinzLinienAlexaSkill.Web.Utility;
+using LinzLinienEfa.Service.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LinzLinienAlexaSkill.Web
+{
+    public class AlexaSkillMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public AlexaSkillMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        
+        public async Task InvokeAsync(HttpContext context, ILogger<AlexaSkillMiddleware> logger, IDeparturesService departuresService, IStopsService stopsService, ILogger<LinzLinienEfaSpeechlet> speechletLogger)
+        {
+            var speechlet = new LinzLinienEfaSpeechlet(speechletLogger, departuresService, stopsService);
+            var response = await speechlet.GetResponseAsync(context.Request.ToHttpRequestMessage());
+            // TODO: Set correct values for non-200 reponses
+            await context.Response.FromHttpResponseMessage(response);
+        }
+    }
+}
---
+++ src/LinzLinienAlexaSkill.Web/AlexaSkillMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace LinzLinienAlexaSkill.Web
+{
+    public static class AlexaSkillMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAlexaSkillMiddleware(this IApplicationBuilder builder, string path)
+        {
+            return builder.Map(path, b => b.UseMiddleware<AlexaSkillMiddleware>());
+        }
+    }
+}
