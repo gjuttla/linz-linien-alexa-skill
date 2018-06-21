6d8720ce19ac881776d64b7f5af1f1bcfe5e8c52
Gurpreet Juttla <gurpreet.juttla@gmail.com>
2018-06-21 18:25:33 +0200

    Remove unnecessary logging

--- src/LinzLinienAlexaSkill.Web/Alexa/AlexaSkillMiddleware.cs
+++ src/LinzLinienAlexaSkill.Web/Alexa/AlexaSkillMiddleware.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using Alexa.NET.Request;
-using Alexa.NET.Response;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -19,22 +18,14 @@ namespace LinzLinienAlexaSkill.Web.Alexa
         
         public async Task InvokeAsync(HttpContext context, ILogger<AlexaSkillMiddleware> logger, SkillRequestHandler skillRequestHandler)
         {
-            logger.LogTrace($"Converting HTTP request body to {nameof(SkillRequest)}");
             string bodyStr;
             using (var reader = new StreamReader(context.Request.Body))
             {
                 bodyStr = reader.ReadToEnd();
             } 
             var skillRequest = JsonConvert.DeserializeObject<SkillRequest>(bodyStr);
-            logger.LogTrace($"Converted HTTP request body to {nameof(SkillRequest)}");
-            
-            logger.LogTrace($"Passing {nameof(SkillRequest)} to {nameof(SkillRequestHandler)}");
             var skillResponse = await skillRequestHandler.HandleRequestAsync(skillRequest);
-            logger.LogTrace($"Got {nameof(SkillResponse)} from {nameof(SkillRequestHandler)}");
-            
-            logger.LogTrace($"Serializing {nameof(SkillResponse)} to JSON");
             await context.Response.WriteAsync(JsonConvert.SerializeObject(skillResponse));
-            logger.LogTrace($"Wrote {nameof(SkillResponse)} as JSON in HTTP response");
         }
     }
 }
